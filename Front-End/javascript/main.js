dffc49158f2efdb8d1d0f6f9cd1a4ca97ae7653f
unknown <wiercikdoc@gmail.com>
2022-10-26 00:05:26 +0200

    Added the listener for input with only hex values

--- Front-End/javascript/main.js
+++ Front-End/javascript/main.js
@@ -1,18 +1,30 @@
-const registerBoxes = [...document.querySelectorAll('.register-box-content')];
+const registerBoxes = [...document.querySelectorAll(".register-box-content")];
+const inputsArray = [...document.querySelectorAll("input")];
 let firstChoosedRegister;
 let secondChoosedRegister;
 registerBoxes.forEach((box) =>
-  box.addEventListener('click', function (e) {
-    if (e.target.localName == 'div') {
+  box.addEventListener("click", function (e) {
+    if (e.target.localName == "div") {
       //   const isMoreTh = counterOfActiveClasses();
       //   isMoreThanTwoActive  > 2? alert("Mozesz wybrac tylko 2 rejestry!") :
-      box.classList.toggle('active');
+      box.classList.toggle("active");
     }
   })
 );
 
+inputsArray.forEach(input => input.addEventListener("input", function(){
+  let regEx = /^[0-9a-fA-F]+$/;
+  let isHex = regEx.test(input.value.toString());
+  if(!isHex) {
+    input.value = input.value.slice(0, -1);
+  }
+
+}))
+
 const counterOfActiveClasses = () => {
   let counter;
-  registerBoxes.forEach((item) => (item == item.classList[1] ? counter++ : false)); //classList[1] active/undefined
+  registerBoxes.forEach((item) =>
+    item == item.classList[1] ? counter++ : false
+  ); //classList[1] active/undefined
   return counter;
 };
