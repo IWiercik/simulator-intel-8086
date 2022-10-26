cdc84f8cc1bd4685faab9fe81be187948c9e53a5
Swiatlon <wiercikdoc@gmail.com>
2022-10-26 11:33:27 +0200

    Added choosing options functioning with maximum amount of choosed options and registers

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,14 @@
-const registerBoxes = [...document.querySelectorAll(".register-box-content")];
-const inputsArray = [...document.querySelectorAll("input")];
+const registerBoxes = [...document.querySelectorAll('.register-box-content')];
+const inputsArray = [...document.querySelectorAll('input')];
+const optionBoxes = [...document.querySelectorAll('.options-box-content')];
+const resultBoxes = [...document.querySelectorAll('.result-box')];
+const operationArrow = document.querySelector('.result-box-arrow');
 let firstChoosedRegister;
 let secondChoosedRegister;
 
-const counterOfActiveClasses = () => {
+const counterOfActiveClasses = (array) => {
   let counter = 0;
-  registerBoxes.forEach((item) => (item.classList[1] ? counter++ : false)); //classList[1] active/undefined
-  console.log(registerBoxes[7].classList[1]);
-  console.log(counter);
+  array.forEach((item) => (item.classList[1] ? counter++ : false)); //classList[1] active/undefined
   return counter;
 };
 
@@ -20,19 +21,19 @@ function listenerOnHexadecimalFormat() {
 }
 
 const graphicChoosingRegister = (e, box) => {
-  if (e.target.localName == "div") {
+  if (e.target.localName == 'div') {
     if (checkIfUserFillInputs()) {
-      if (counterOfActiveClasses() == 2) {
+      if (counterOfActiveClasses(registerBoxes) == 2) {
         if (box.classList[1]) {
-          box.classList.remove("active");
+          box.classList.remove('active');
         } else {
-          alert("Osiagnieto maksymalna ilosc!");
+          alert('Osiagnieto maksymalna ilosc!');
         }
       } else {
-        box.classList.toggle("active");
+        box.classList.toggle('active');
       }
     } else {
-      alert("First you need to fill all inputs to choose register!");
+      alert('First you need to fill all inputs to choose register!');
     }
   }
 };
@@ -40,15 +41,31 @@ const checkIfUserFillInputs = () => {
   const areAllFieldsFilledCorrectly = inputsArray.every((item) => {
     return item.value.length == 2;
   });
-  console.log(areAllFieldsFilledCorrectly);
+  return areAllFieldsFilledCorrectly;
 };
+function choosingOption() {
+  if (!checkIfUserFillInputs()) {
+    alert('First you need to fill all inputs to choose option!');
+  } else if (counterOfActiveClasses(registerBoxes) != 2) {
+    alert('First you need to choose 2 registers!');
+  } else {
+    if (counterOfActiveClasses(optionBoxes) == 1) {
+      if (this.classList[1]) {
+        this.classList.remove('active-option');
+      } else {
+        alert('Osiagnieto maksymalna ilosc operacji!');
+      }
+    } else {
+      this.classList.toggle('active-option');
+    }
+  }
+}
 
 registerBoxes.forEach((box) =>
-  box.addEventListener("click", function (e) {
+  box.addEventListener('click', function (e) {
     graphicChoosingRegister(e, box);
   })
 );
 
-inputsArray.forEach((input) =>
-  input.addEventListener("input", listenerOnHexadecimalFormat)
-);
+inputsArray.forEach((input) => input.addEventListener('input', listenerOnHexadecimalFormat));
+optionBoxes.forEach((box) => box.addEventListener('click', choosingOption));
