a5b2d851cb22e8b6067aeb4d9289564bac94392e
unknown <wiercikdoc@gmail.com>
2022-10-27 02:53:59 +0200

    Repaired choosing register function and added showing the actual choosed register

--- javascript/main.js
+++ javascript/main.js
@@ -3,9 +3,8 @@ const inputsArray = [...document.querySelectorAll("input")];
 const optionBoxes = [...document.querySelectorAll(".options-box-content")];
 const resultBoxes = [...document.querySelectorAll(".result-box")];
 const operationArrow = document.querySelector(".result-box-arrow");
-let firstChoosedRegister;
-let secondChoosedRegister;
-
+const submitButton = document.querySelector(".confirm-operation");
+const registers = ["", ""]; // "" = free place for register
 const counterOfActiveClasses = (array) => {
   let counter = 0;
   array.forEach((item) => (item.classList[1] ? counter++ : false)); //classList[1] active/undefined
@@ -18,17 +17,25 @@ function listenerOnHexadecimalFormat() {
     this.value = this.value.slice(0, -1);
   }
 }
-const graphicChoosingRegister = (e, box) => {
+const choosingRegister = (e, box) => {
   if (e.target.localName == "div") {
     if (checkIfUserFillInputs()) {
-      if (counterOfActiveClasses(registerBoxes) == 2) {
-        if (box.classList[1]) {
-          box.classList.remove("active");
-        } else {
-          alert("Osiagnieto maksymalna ilosc!");
-        }
+      if (box.classList[1]) {
+        // removing
+        const indexItemToRemove = registers.indexOf(box.textContent);
+        registers[indexItemToRemove] = "";
+        box.classList.remove("active");
       } else {
-        box.classList.toggle("active");
+        // ADDING
+        if (counterOfActiveClasses(registerBoxes) != 2) {
+          const firstFreeSpace = registers.indexOf("");
+          if (firstFreeSpace != -1) {
+            registers[firstFreeSpace] = box.textContent;
+          }
+          box.classList.add("active");
+        } else {
+          alert("2 registers are maximum!");
+        }
       }
     } else {
       alert("First you need to fill all inputs to choose register!");
@@ -56,14 +63,19 @@ function choosingOption() {
       }
     } else {
       this.classList.toggle("active-option");
-      operationArrow.textContent = this.attributes[1].value;
+      operationArrow.innerHTML = this.attributes[1].value;
     }
   }
 }
+const refreshingResultBoxesValues = () => {
+  resultBoxes[0].textContent = registers[0];
+  resultBoxes[1].textContent = registers[1];
+};
 
 registerBoxes.forEach((box) =>
   box.addEventListener("click", function (e) {
-    graphicChoosingRegister(e, box);
+    choosingRegister(e, box);
+    refreshingResultBoxesValues();
   })
 );
 
