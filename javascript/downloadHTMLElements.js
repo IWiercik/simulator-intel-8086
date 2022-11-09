1ea05a947623c01931b2a1d9900d028ce8892b5f
Swiatlon <wiercikdoc@gmail.com>
2022-11-09 12:01:31 +0100

    Deleted old code

--- javascript/downloadHTMLElements.js
+++ javascript/downloadHTMLElements.js
@@ -5,12 +5,10 @@ const registerInputsArray = [...document.querySelectorAll('.register-input')];
 const operationBoxes = [...document.querySelectorAll('.options-box-content')];
 const operationPlace = document.querySelector('.operation-place');
 // BOTTOM SIDE OPERATION
-const operaionUsedMultiChildWrapper = document.querySelector('.multi-operation');
-const operationUsedBoxes = [...document.querySelectorAll('#multi-box')];
+const operationUsedMultiChildWrapper = document.querySelector('.multi-operation');
 const operationArrow = document.querySelector('.result-box-arrow');
 const operationUsedSingleChildWrapper = document.querySelector('.single-operation');
-const operationUsedSingleBox = document.querySelector('#single-box');
-const operationInput = document.querySelector('#single-box-input');
+const operationInputs = [...document.querySelectorAll('input')];
 // OPERATION DATA TYPES
 let dataTypesDivs = [...document.querySelectorAll('.data-type-wrapper')].map((item) => [
   item.children[0],
@@ -22,7 +20,7 @@ const submitButton = document.querySelector('.confirm-operation');
 
 // EVENT LISTENERS
 registerInputsArray.forEach((input) => input.addEventListener('input', listenerOnHexadecimalFormat));
-operationInput.addEventListener('input', listenerOnHexadecimalFormat);
+operationInputs.forEach((input) => input.addEventListener('input', listenerOnHexadecimalFormat));
 operationBoxes.forEach((box) => box.addEventListener('click', choosingOperation));
 dataTypesDivs.forEach((array, parentIndex) =>
   array.forEach((item, boxIndex) =>
