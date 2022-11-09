a9662fac26147a73bebcb4a8ca63a5e750b8a0b9
Swiatlon <wiercikdoc@gmail.com>
2022-11-09 22:48:02 +0100

    Refactored choosing elements

--- javascript/main.js
+++ javascript/main.js
@@ -1,36 +1,24 @@
-const registers = ['', '']; // "" = free place for register
-const dataBoxes = ['', ''];
+let registers = [];
 let choosedDataType = [];
 let singleDataType = false;
 let operation;
 
+//BASIC FUNCTIONS
 function choosingDataType(box, parent, parentIndex, boxIndex) {
-  if (counterOfActiveClasses(parent) == 0) {
-    //
-    box.classList.add('active-data-type');
-    cursivesBettwenDataTypes[parentIndex].style.background = `linear-gradient(125deg, ${
-      boxIndex == 0 ? 'green' : 'black'
-    } 50%, rgba(9, 9, 121, 1) 40%, ${boxIndex == 0 ? 'black' : 'green'} 10%)`;
-    manageDataDivsContent(box, parent);
-  } else if (counterOfActiveClasses(parent) == 1) {
-    if (box.classList[1]) {
-      //if user clicked on actived item
-      box.classList.remove('active-data-type');
-      cursivesBettwenDataTypes[
-        parentIndex
-      ].style.background = `linear-gradient(125deg, rgba(2, 0, 36, 1) 50%, rgba(9, 9, 121, 1) 40%, black 10%)`;
-      manageDataDivsContent(box, parent);
-    } else {
-      showingSweetAlert('Maximum data types', 'Choose only 1 type of data cell or register');
-    }
-  }
+  parent.forEach((item) => item.classList.remove('active-data-type'));
+  box.classList.add('active-data-type');
+  cursivesBettwenDataTypes[parentIndex].style.background = `linear-gradient(125deg, ${
+    boxIndex == 0 ? 'green' : 'black'
+  } 50%, rgba(9, 9, 121, 1) 40%, ${boxIndex == 0 ? 'black' : 'green'} 10%)`;
+  manageDataDivsContent(box, parent);
   choosedDataType = gettingActiveDataTypes(dataTypesDivs);
+  deleteAllActiveRegisters();
 }
 function choosingOperation() {
   if (!checkIfUserFillInputs()) {
     showingSweetAlert('Fill Inputs', 'First you need to fill all inputs and choosed data type register or cell');
   } else {
-    operationBoxes.forEach((box) => box.classList.remove('active-option'));
+    optionsBoxes.forEach((box) => box.classList.remove('active-option'));
     this.classList.add('active-option');
     operationArrow.innerHTML = this.attributes[1].value; // we update operation Arrow with data-content value
     operation = this.textContent;
@@ -52,17 +40,40 @@ function choosingOperation() {
   }
   refetchDataTypes();
   choosedDataType = gettingActiveDataTypes(dataTypesDivs);
+  deleteAllActiveRegisters();
+  refetchOperationBoxesValue();
 }
 const choosingRegister = (e, box) => {
-  if (e.target.localName == 'div') {
+  const clickedDiv = e.target;
+  const maximumRegisterBasedOnDataType = choosedDataType.filter((item) => item.textContent == 'REGISTER').length;
+  if (clickedDiv.localName == 'div') {
     if (checkIfUserFillInputs()) {
-      if (!singleDataType) {
+      if (!clickedDiv.classList[1]) {
+        if (registers.length < maximumRegisterBasedOnDataType) {
+          clickedDiv.classList.add('active');
+          refetchOperationBoxesValue();
+          // operationBoxes.forEach((HtmlElement, index) => {
+          //   if (HtmlElement.localName == 'span') {
+          //     HtmlElement.textContent = `${clickedDiv.textContent.trim().slice(0, -1)}`; // Span getting value of register input value;
+          //     registers[index] = clickedDiv.children[0].value;
+          //   }
+          // });
+          // for(let i =0 ; i<)
+          console.log(operationBoxes);
+        } else {
+          showingSweetAlert(
+            'Maxium amount of registers',
+            'You reached maximum amount of register based on choosed data-type'
+          );
+        }
       } else {
+        clickedDiv.classList.remove('active');
       }
     } else {
       showingSweetAlert('Fill Inputs', 'First you need to fill all inputs');
     }
   }
+  registers = [...document.querySelectorAll('.register-box-wrapper .active')];
 };
 const submittingProgram = () => {
   let firstRegister;
@@ -77,7 +88,7 @@ const submittingProgram = () => {
       }
     }
   });
-  operationBoxes.forEach((option) => (option.classList[1] ? (operation = option.textContent) : false));
+  optionsBoxes.forEach((option) => (option.classList[1] ? (operation = option.textContent) : false));
   const isValid = firstRegister?.value.length > 0 && secondRegister?.value.length > 0 && operation.length > 0;
   if (isValid) {
     switch (operation) {
@@ -95,6 +106,7 @@ const submittingProgram = () => {
     showingSweetAlert('Choose elements!', 'First you need to choose registers and operation!');
   }
 };
+// ADDITIONAL + SECURE
 const refetchDataTypes = () => {
   // we use it bettwen switching operation
   dataTypesDivs = [...document.querySelectorAll('.visible .data-type-wrapper')].map((item) => [
@@ -102,17 +114,20 @@ const refetchDataTypes = () => {
     item.children[2],
   ]);
 };
+const refetchOperationBoxesValue = () => {
+  operationBoxes = [...document.querySelectorAll('.visible .result-box .box-value')];
+};
 const manageDataDivsContent = (element, parent) => {
   const elementWrapper = element.parentElement.parentElement;
   let activeDiv = parent.filter((div) => div.classList[1]);
-  if (activeDiv[0]?.textContent == 'CELL') {
-    if (elementWrapper.children.length > 2) {
-      elementWrapper.removeChild(elementWrapper.children[2]);
-    }
-  } else if (activeDiv[0]?.textContent == 'REGISTER') {
-    if (elementWrapper.children.length == 2) {
-      elementWrapper.appendChild(createInput());
-    }
+  if (activeDiv[0]?.textContent == 'REGISTER') {
+    //Remove last child
+    elementWrapper.removeChild(elementWrapper.lastElementChild);
+    elementWrapper.appendChild(createSpan());
+  } else if (activeDiv[0]?.textContent == 'CELL') {
+    //Remove all childs
+    elementWrapper.removeChild(elementWrapper.lastElementChild);
+    elementWrapper.appendChild(createInput());
   }
 };
 const checkIfUserFillInputs = () => {
@@ -151,14 +166,28 @@ function createInput() {
   Object.assign(input, {
     type: 'text',
     name: 'register-value',
-    maxLength:4,
+    className: 'box-value',
+    maxLength: 4,
     minLength: 4,
     required: true,
   });
-  input.addEventListener('input',listenerOnHexadecimalFormat);
+  input.addEventListener('input', listenerOnHexadecimalFormat);
   return input;
+}
+function createSpan() {
+  const span = document.createElement('span');
+  span.setAttribute('class', 'box-value');
+  return span;
+}
+function deleteAllActiveRegisters() {
+  registers.forEach((item) => item.classList.remove('active'));
+  registers = [];
+}
+function gettingFirstEmptySpan(array){
+  // array.forEach( htmlElement => htmlElement.)
 }
 //Config for loading website;
 window.addEventListener('load', function () {
   refetchDataTypes();
+  choosedDataType = gettingActiveDataTypes(dataTypesDivs);
 });
