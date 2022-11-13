ebb183639ab3df602d5183c708f19cb07250788d
unknown <wiercikdoc@gmail.com>
2022-11-13 13:15:17 +0100

    Created schema for showing results

--- javascript/sweetAlerts.js
+++ javascript/sweetAlerts.js
@@ -7,3 +7,22 @@ function showingSweetAlert(title, text) {
     confirmButtonText: "Ok!",
   });
 }
+function showingOperationsResult(operation,firstValueBefore,secondValueBefore,firstValueAfter,secondValueAfter){
+  Swal.fire({
+    html:`
+    <div class="sweet-alert-result-wrapper">
+      <h1>Operation: <span class="green-string">${operation}</span> </h1> 
+      <h2>Before:</h2>
+      <div class="results-wrapper">
+        <div>${firstValueBefore.name}:<span class="green-string">${firstValueBefore.value}</span></div>
+        ${secondValueAfter ? `<div>${secondValueBefore.name}: <span class="green-string">${secondValueBefore.value}</span></div>` : ""}
+      </div>
+      <h2>After:</h2>
+      <div class="results-wrapper">
+      <div>${firstValueAfter.name}:<span class="green-string">${firstValueAfter.value}</span></div>
+      ${secondValueAfter ? `<div>${secondValueAfter.name}: <span class="green-string">${secondValueAfter.value}</span></div>` : ""}
+      </div>
+    </div>
+    `,
+  })
+}
