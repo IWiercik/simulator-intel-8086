168532ec5a23f3952647f30168986fa5ce0dbbe9
unknown <wiercikdoc@gmail.com>
2022-11-13 13:14:28 +0100

    Created files for managing cells

---
+++ javascript/cells.js
@@ -0,0 +1,25 @@
+const cells = [];
+const gettingRandomHexValue =() =>{
+ let randomNumber = Math.random()*256|0;
+  randomNumber = randomNumber.toString(16).toUpperCase();
+  if(randomNumber.length <2){
+    randomNumber = "0" + randomNumber;
+  }
+  return randomNumber;
+}
+
+const fillingCells = async () =>{
+    for(let i= 0 ; i <65536; i++){
+        let name = i.toString(16).toUpperCase() ;
+        while(name.length < 4) name = "0" + name;
+        const  obj = {
+          name: name,
+          value : gettingRandomHexValue(),
+        };
+        cells.push(obj);
+      }
+}
+const workingOnCell = (name) =>{
+    const cell = cells.find(cell => cell.name === name);
+    return cell;
+}
