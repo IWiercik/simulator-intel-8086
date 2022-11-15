0658641e536e689181c808c06a019754a037a3a0
unknown <wiercikdoc@gmail.com>
2022-11-15 12:03:20 +0100

    Refactored and cleaned code

--- front-end-version/javascript/DOM.js
+++ front-end-version/javascript/DOM.js
@@ -1,6 +1,6 @@
 // TOP SIDE REGISTERS
 const registerBoxes = [...document.querySelectorAll('.register-box-content')];
-const registerInputsArray = [...document.querySelectorAll('.register-input')];
+const registersInputs = [...document.querySelectorAll('.register-input')];
 // MIDDLE SIDE OPTIONS
 const optionsBoxes = [...document.querySelectorAll('.options-box-content')];
 const operationPlace = document.querySelector('.operation-place');
@@ -8,11 +8,10 @@ const operationPlace = document.querySelector('.operation-place');
 const operationUsedMultiChildWrapper = document.querySelector('.multi-operation');
 const operationArrow = document.querySelector('.result-box-arrow');
 const operationUsedSingleChildWrapper = document.querySelector('.single-operation');
-const operationInputs = [...document.querySelectorAll('input')];
 // OPERATION DATA TYPES
 let dataTypesDivs = [...document.querySelectorAll('.data-type-wrapper')].map((item) => [
-  item.children[0],
-  item.children[2],
+  item.children[0], //REGISTER
+  item.children[2], // CELL
 ]);
 const cursivesBettwenDataTypes = [...document.querySelectorAll('.cursive-parent')];
 // ENDING PROGRAM
@@ -20,8 +19,7 @@ let operationBoxes = [...document.querySelectorAll('.visible .result-box .box-va
 const submitButton = document.querySelector('.confirm-operation');
 
 // EVENT LISTENERS
-registerInputsArray.forEach((input) => input.addEventListener('input', listenerOnHexadecimalFormat));
-operationInputs.forEach((input) => input.addEventListener('input', listenerOnHexadecimalFormat));
+registersInputs.forEach((input) => input.addEventListener('input', listenerOnHexadecimalFormat));
 optionsBoxes.forEach((box) => box.addEventListener('click', choosingOperation));
 dataTypesDivs.forEach((array, parentIndex) =>
   array.forEach((item, boxIndex) =>
@@ -38,5 +36,41 @@ registerBoxes.forEach((box) =>
     event.stopPropagation();
   })
 );
-
 submitButton.addEventListener('click', submittingProgram);
+//DOM FUNCTIONS
+const refetchDataTypes = () => {
+  // we use it bettwen switching operation
+  dataTypesDivs = [...document.querySelectorAll('.visible .data-type-wrapper')].map((item) => [
+    item.children[0],
+    item.children[2],
+  ]);
+};
+const refetchOperationBoxesValue = () => {
+  operationBoxes = [...document.querySelectorAll('.visible .result-box .box-value')];
+};
+function createInput() {
+  const input = document.createElement('input');
+  Object.assign(input, {
+    type: 'text',
+    name: 'register-value',
+    className: 'box-value',
+    maxLength: 4,
+    minLength: 4,
+    required: true,
+  });
+  input.addEventListener('input', listenerOnHexadecimalFormat);
+  return input;
+}
+function createSpan() {
+  const span = document.createElement('span');
+  span.setAttribute('class', 'box-value');
+  return span;
+}
+// SECURITY
+function listenerOnHexadecimalFormat() {
+  let regEx = /^[0-9a-fA-F]+$/;
+  let isHex = regEx.test(this.value.toString());
+  if (!isHex) {
+    this.value = this.value.slice(0, -1);
+  }
+}
